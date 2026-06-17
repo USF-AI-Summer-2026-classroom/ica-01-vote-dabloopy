331e8645a956d72b6d28a1cf73567d4130087179
Brian Lopez <lopezrocha3531@gmail.com>
2026-06-16 22:28:14 -0700

    [1.2] - Added elec func. and loop till winner is found

--- voting_system.py
+++ voting_system.py
@@ -28,6 +28,39 @@ class VotingSystem:
             for i in range(count)
         ]
 
+    def run_election(self, eliminated_candidates):
+        # results is dictionary W/ candidate names & zero votes
+        results = {candidate.name: 0 for candidate in self.candidates}
+
+        #loop through voters -> get top choice using queue method & count votes
+        for voter in self.voters:
+            candidate_choice = voter.get_candidate(eliminated_candidates)  
+            if candidate_choice:
+                results[candidate_choice.name] += 1
+        return results
+    
+    def election_results(self, results, eliminated_candidates):
+        current_results = {
+            name: votes for name, votes in results.items()
+            if name not in eliminated_candidates
+        }
+
+        total_curr_votes = sum(current_results.values())
+
+        if total_curr_votes == 0:
+            return [], 0.0, [], 0.0
+
+        high_vote = max(current_results.values())
+        low_vote = min(current_results.values())
+
+        max_votes = (high_vote / total_curr_votes) * 100
+        min_votes = (low_vote / total_curr_votes) * 100
+
+        winner = [name for name, votes in current_results.items() if votes == high_vote]
+        loser = [name for name, votes in current_results.items() if votes == low_vote]
+
+        return winner, max_votes, loser, min_votes
+
 if __name__ == "__main__":
     voting_system = VotingSystem()
 
@@ -41,3 +74,47 @@ if __name__ == "__main__":
     print("\nVoters:")
     for voter in voting_system.voters:
         print(voter)
+
+    #initialize voter queues
+    for voter in voting_system.voters:
+        voter.generate_rankings(voting_system.candidates)
+
+    winner_found = False
+    eliminated_candidates = []
+    rounds = 0
+    #run election rounds until a winner is found
+    while not winner_found:
+        rounds += 1
+        print(f"\n----------- Election Has Started -----------  ")
+        #get election results, determine winners & losers
+        results = voting_system.run_election(eliminated_candidates)
+        winner, max_votes, loser, min_votes = voting_system.election_results(results, eliminated_candidates)
+
+        #error detection
+        if not winner:
+            print("No votes or candidate available")
+            break
+
+        #if winner is found
+        if max_votes > 50.0:
+            winner_found = True
+            print(f"\nMajority has been reached! Round {rounds} standings:")
+            for name, votes in results.items():
+                if name in eliminated_candidates:
+                    continue
+                else:
+                    percent = (votes / len(voting_system.voters)) * 100
+                    print(f"\n  - {name} got {percent:.2f}% of votes")
+            print(f"\nThe winner is {winner[0]} with {max_votes:.2f}% of the vote")
+        
+        else:
+            print(f"\nMajority was not reached! Round {rounds} standings:")
+            for name, votes in results.items():
+                if name in eliminated_candidates:
+                    continue
+                else:
+                    percent = (votes / len(voting_system.voters)) * 100
+                    print(f"\n  - {name} got {percent:.2f}% of votes")
+            print(f"\nEliminating {loser[0]} with only {min_votes:.2f}% of votes")
+            eliminated_candidates.extend(loser)
+

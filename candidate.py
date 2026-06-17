fb4b02800144ed4927122b7772fb553112dff18d
Brian Lopez <lopezrocha3531@gmail.com>
2026-06-16 18:19:35 -0700

    [01.0] - added get/set for eleminated bool

--- candidate.py
+++ candidate.py
@@ -3,6 +3,8 @@ class Candidate:
     def __init__(self, name, leaning):
         self.name = name
         self.leaning = leaning
+        #adding eleiminated status to candidate
+        self.eliminated = False
 
     def get_leaning(self):
         return self.leaning
@@ -15,4 +17,16 @@ class Candidate:
             f"Candidate = {self.name}, "
             f"leaning: {self.leaning:.3f}"
         )
+    
+    def is_eliminate(self):
+        return self.eliminated
 
+    def eliminate(self):
+        self.eliminated = True
+
+    def __repr__(self):
+        return {
+            "name": self.name,
+            "leaning": self.leaning,
+            "eliminated": self.eliminated
+        }
